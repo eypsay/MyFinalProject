d1071a61f0b781e2e58598afe1ca7ca024109872
eyp <eypsay@yahoo.com>
2021-02-06 22:52:40 +0300

    Core katmanı eklendi

--- DataAccess/Abstract/IProductDal.cs
+++ DataAccess/Abstract/IProductDal.cs
@@ -1,4 +1,5 @@
-﻿using Entities.Concrete;
+﻿using Core.DataAccess;
+using Entities.Concrete;
 using System;
 using System.Collections.Generic;
 using System.Text;
@@ -9,7 +10,7 @@ namespace DataAccess.Abstract
     //product tablosuna iat interface
     public interface IProductDal:IEntityRepository<Product>
     {
-      
+      //Buraya product a öozel işlemeri yazacağız. prduct detail gibi 
 
     }
 }
